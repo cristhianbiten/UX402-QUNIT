c3d1e4e303c7a8c8d832de5cfcd1845095f9d414
Cristhian Bitencourt <cristhianthiagok@yahoo.com>
2024-11-16 17:09:03 -0300

    first commit

---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,7 @@
+sap.ui.define([
+	"student10/com/sap/training/ux402/qunit/ux402_qunit/test/unit/controller/Main.controller",
+	"student10/com/sap/training/ux402/qunit/ux402_qunit/test/unit/model/Formatter"
+   ], function () {
+	"use strict";
+   });
+   
